93c7f567ec0c6a7a1bdf49e5246ff51bbd3a1b8d
klose911 <klose911@gmail.com>
2016-11-14 15:04:15 +0800

    merge org, css ,pic files into new reposiotry

---
+++ src/export.el
@@ -0,0 +1,46 @@
+(require 'ox-publish)
+(setq org-publish-project-alist
+      '(
+        ;; These are the main web files
+        ("notes-html"
+         :base-directory "~/Documents/programming/html/klose911.github.io/src" 
+         :base-extension "org"
+         :publishing-directory "~/Documents/programming/html/klose911.github.io/html/material/"
+         :recursive nil
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4
+         :auto-sitemap nil
+	 :section-numbers nil
+         ;; :with-toc nil
+         :html-head "<link rel=\"stylesheet\"
+                       href=\"css/main.css\" type=\"text/css\"/>"
+         :html-preamble t
+	 :htmlized-source t
+         )
+
+        ;; css
+        ("notes-css"
+         :base-directory "~/Documents/programming/html/klose911.github.io/src/css" ;; Change this to your local dir
+         :base-extension "css"
+         :publishing-directory "~/Documents/programming/html/klose911.github.io/html/material/css"
+         :recursive t
+         :publishing-function org-publish-attachment
+         )
+
+	;; picture
+        ("notes-pic"
+         :base-directory "~/Documents/programming/html/klose911.github.io/src/pic"  ;; Change this to your local dir
+         :base-extension "png\\|jpg\\|gif"
+         :publishing-directory "~/Documents/programming/html/klose911.github.io/html/material/pic"
+         :recursive t
+         :publishing-function org-publish-attachment
+         )
+	
+        ("note" :components ("notes-css" "notes-pic" "notes-html"))
+        )
+      )
+
+(defun note-publish nil
+  "Publish notes"
+  (interactive)
+  (org-publish "note" nil))
