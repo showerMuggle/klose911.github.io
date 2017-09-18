39fb6ea8c84c6c6ab4836682ae90b11f17e96409
klose911 <klose911@gmail.com>
2017-09-18 16:27:12 +0800

    performance, decorator, context-manager, concision

--- src/python/src/pythonic/readablity/unpack.py
+++ src/python/src/pythonic/readablity/unpack.py
@@ -4,7 +4,7 @@
 
 p =  'Raymond', 'Hettinger',  0x30, 'python@example.com'
 
-('Raymond', 'Hettinger', 48, 'python@example.com')
+#('Raymond', 'Hettinger', 48, 'python@example.com')
 
 # 其它语言的常用方法/习惯 
 fname = p[0]
@@ -12,3 +12,8 @@ lname = p[1]
 age = p[2]
 email = p[3]
 
+fname, lname, age, email = p
+print(fname)
+print(lname)
+print(age)
+print(email) 
