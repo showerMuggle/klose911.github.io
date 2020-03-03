56a3ad0f0d59bde7a58d50d0f2d6a41dac27712d
klose911 <klose911@gmail.com>
2020-03-03 17:10:52 +0800

    trait bounds and conditional traits

--- src/rust/src/generic/function/src/main.rs
+++ src/rust/src/generic/function/src/main.rs
@@ -37,7 +37,7 @@
 // }
 
 
-fn largest<T>(list: &[T]) -> T {
+fn largest<T:PartialOrd+Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
--- src/rust/src/trait/summary/src/main.rs
+++ src/rust/src/trait/summary/src/main.rs
@@ -66,6 +66,35 @@ pub fn notify(item: impl Summary) {
     println!("Breaking news! {}", item.summarize());
 }
 
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+// could return only one exactly type 
+// fn returns_summarizable(switch: bool) -> impl Summary {
+//     if switch {
+//         NewsArticle {
+//             headline: String::from("Penguins win the Stanley Cup Championship!"),
+//             location: String::from("Pittsburgh, PA, USA"),
+//             author: String::from("Iceburgh"),
+//             content: String::from("The Pittsburgh Penguins once again are the best
+//             hockey team in the NHL."),
+//         }
+//     } else {
+//         Tweet {
+//             username: String::from("horse_ebooks"),
+//             content: String::from("of course, as you probably already know, people"),
+//             reply: false,
+//             retweet: false,
+//         }
+//     }
+// }
+
 fn main() {
     let tweet = Tweet {
 	username: String::from("horse_ebooks"),
