6755649d95496fe0ea40ac4ddf19b9933448e4ba
klose911 <klose911@gmail.com>
2020-03-07 21:48:52 +0800

    lifetime annotaion of function's parameter

--- src/rust/src/lifetime/longest/src/main.rs
+++ src/rust/src/lifetime/longest/src/main.rs
@@ -1,4 +1,4 @@
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
@@ -6,10 +6,27 @@ fn longest(x: &str, y: &str) -> &str {
     }
 }
 
-fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+// 即使指定了声明周期，仍然无法返回一个悬垂引用
+// fn longest<'a>(x: &str, y: &str) -> &'a str {
+//     let result = String::from("really long string");
+//     result.as_str()
+// }
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+fn main() {
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    // 返回值的生命周期是两个实参的具体声明周期里更短的一个
+    // let string1 = String::from("long string is long");
+    // let result;
+    // {
+    //     let string2 = String::from("xyz");
+    //     result = longest(string1.as_str(), string2.as_str());
+    // }
+    // println!("The longest string is {}", result);
 }
