e6909692c357005b8254c57f2913e4df20ca14d2
klose911 <klose911@gmail.com>
2020-03-08 21:14:15 +0800

    lifetime of struct and method

--- src/rust/src/lifetime/longest/src/main.rs
+++ src/rust/src/lifetime/longest/src/main.rs
@@ -12,6 +12,19 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 //     result.as_str()
 // }
 
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
     let string1 = String::from("long string is long");
 
