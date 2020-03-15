e0a3e34c0c1f585b8f78ba3dc9855da5029088ba
klose911 <klose911@gmail.com>
2020-03-15 21:53:36 +0800

    split main logic and return Result instead of panic

--- src/rust/src/minigrep/src/main.rs
+++ src/rust/src/minigrep/src/main.rs
@@ -1,32 +1,57 @@
 use std::env;
 use std::fs; 
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //println!("{:?}", args);
-    let config = Config::new(&args);
 
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
 
-    println!("With text:\n{}", contents);
+        process::exit(1);
+    }
 }
 
+// fn run(config: Config) {
+//     let contents = fs::read_to_string(config.filename)
+//         .expect("Something went wrong reading the file");
+
+//     println!("With text:\n{}", contents);
+// }
+
+use std::error::Error;
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
 
 struct Config {
     query: String,
     filename: String,
 }
 
+
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
