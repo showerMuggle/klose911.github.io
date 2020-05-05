a3e8f6628fa8cfb76b109bd6c60570daa6d22822
klose911 <klose911@gmail.com>
2020-05-05 16:22:27 +0800

    Reviewed-by: klose911 <klose911@gmail.com>

---
+++ src/rust/src/box_pointer/src/main.rs
@@ -0,0 +1,15 @@
+use crate::List::{Cons, Nil};
+
+fn main() {
+    let b = Box::new(5);
+    println!("b = {}", b);
+
+    let list = Cons(1, Cons(2, Cons(3, Nil)));
+
+}
+
+enum List {
+    Cons(i32, List),
+    Nil,
+}
+
