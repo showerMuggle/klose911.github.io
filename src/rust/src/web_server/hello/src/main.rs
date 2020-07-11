563a4cdde23d7fa80975527bb87a19a47dfa7eff
klose911 <klose911@gmail.com>
2020-07-11 17:22:49 +0800

    simulate slow http request

--- src/rust/src/web_server/hello/src/main.rs
+++ src/rust/src/web_server/hello/src/main.rs
@@ -2,7 +2,8 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 use std::fs;
-
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -21,6 +22,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     // if buffer.starts_with(get) {
     // 	let contents = fs::read_to_string("hello.html").unwrap();
@@ -40,6 +42,9 @@ fn handle_connection(mut stream: TcpStream) {
     // }
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
