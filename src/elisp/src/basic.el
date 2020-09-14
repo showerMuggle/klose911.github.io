3ac212172702ef3ea5f2822aadcf752fdcf7e5ea
klose911 <klose911@gmail.com>
2020-09-14 17:48:22 +0800

    control flow

--- src/elisp/src/basic.el
+++ src/elisp/src/basic.el
@@ -46,4 +46,31 @@ bar
 ;;;;;;;;;;;;;;;;;;
 ;; control flow ;;
 ;;;;;;;;;;;;;;;;;;
-;;
+;;; sequential
+(progn
+  (setq foo 3)
+  (message "Square of %d is %d" foo (* foo foo)))
+
+;;; condition
+(defun my-max (a b)
+  (if (> a b)
+      a b))
+(my-max 3 4)                            ; => 4
+
+(defun fib (n)
+  (cond ((= n 0) 0)
+	((= n 1) 1)
+	(t (+ (fib (- n 1))
+	      (fib (- n 2))))))
+(fib 10)                                ; => 55
+
+;;; loop
+(defun factorial (n)
+  (let ((res 1))
+    (while (> n 1)
+      (setq res (* res n)
+            n (- n 1)))
+    res))
+(factorial 10)      ;; => 55 
+
+
