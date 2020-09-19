d28b189bff6f1982b51c7da3d31685e220867d8b
klose911 <klose911@gmail.com>
2020-09-19 21:22:17 +0800

    test and constructor of cons and list

--- src/elisp/src/data-type/list.el
+++ src/elisp/src/data-type/list.el
@@ -1,5 +1,5 @@
 ;;;;;;;;;;;;;;;;
-;; cons cell  ;;
+;; list       ;;
 ;;;;;;;;;;;;;;;;
 
 ;;; read 
@@ -25,3 +25,47 @@ nil                                     ; => nil
 
 
 '(1 . (2 . (3 . nil)))                  ; => (1 2 3)
+
+;;; test function
+(consp '(1 . 2))                        ; => t
+(consp '(1 . (2 . nil)))                ; => t
+(consp nil)                             ; => nil
+(listp '(1 . 2))                        ; => t
+(listp '(1 . (2 . nil)))                ; => t
+(listp nil)                             ; => t
+
+(null '()) ;; => t
+(null nil) ;; => t
+(null '(1 2) ;; => nil
+
+      
+;;; constructor
+
+;; cons 
+(cons 1 2)                              ; => (1 . 2)
+(cons 1 '())                            ; => (1)
+
+(setq foo '(a b))                       ; => (a b)
+(cons 'x foo)                           ; => (x a b)
+foo
+					; => (a b)
+(push 'x foo)                           ; => (x a b)
+foo                                     ; => (x a b)
+
+;; list 
+(list 1 2 3)                            ; => (1 2 3)
+
+;; quote
+'((+ 1 2) 3)                            ; => ((+ 1 2) 3)
+(list (+ 1 2) 3)                        ; => (3 3)
+
+;; append
+(append '(a b) '(c))                    ; => (a b c)
+(append '(a b) '(c) '(d))               ; => (a b c d)
+(append '(a b) 'c)                      ; => (a b . c)
+
+(append [a b] "cd" nil)                 ; => (a b 99 100)
+
+;;;;;;;;;;;;
+;; array  ;;
+;;;;;;;;;;;;
